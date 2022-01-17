152336d2329d7a6594e59dd601bdc380e7344db6
Annika Hägglund <annika.a.a.hagglund@outlook.com>
2022-01-17 11:09:23 +0100

    added jsonwebtoken and changes to /authorize

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const credentials = { secretUser: "user", secretPassword: "password" };
+
 const cors = require("cors");
 const express = require("express");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
 
-const res = require("express/lib/response");
 const app = express();
-process.env.PORT = 3000;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use(express.json());
+const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next) {
   res.setHeader(
@@ -18,9 +16,11 @@ app.use(function (req, res, next) {
   next();
 });
 
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
 app.get("/", (req, res) => {
   const encodedAuth = (req.headers.authorization || "").split(" ")[1] || ""; // getting the part after Basic
-
   const [user, password] = Buffer.from(encodedAuth, "base64")
     .toString()
     .split(":");
@@ -37,26 +37,38 @@ app.get("/", (req, res) => {
 });
 
 app.get("/health", (req, res) => {
-  headers = { http_status: 200, "cache-control": "no-chache" };
+  headers = { http_status: 200, "cache-control": "no-cache" };
   body = { status: "available" };
 });
 
 app.post("/authorize", (req, res) => {
-  //insert login code here
+  // Insert Login Code Here
   let user = req.body.user;
   let password = req.body.password;
-  console.log(`User $(user)`);
-  console.log(`Password $(password)`);
+  console.log(`User ${user}`);
+  console.log(`Password ${password}`);
 
   if (
     user === credentials.secretUser &&
     password === credentials.secretPassword
   ) {
     console.log("Authorized");
-    res.status(200).send({ STATUS: "SUCCESS" });
+    const token = jwt.sign(
+      {
+        data: "foobar",
+      },
+      "your-secret-key-here",
+      { expiresIn: 60 * 60 }
+    );
+
+    console.log(token);
+    res.status(200).send(token);
+  } else {
+    console.log("Not authorized");
+    res.status(200).send({ STATUS: "FAILURE" });
   }
 });
 
-app.listen(3000, () => {
-  console.log(`STARTED LISTENING ON PORT ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`STARTED LISTENING ON PORT ${PORT}`);
 });
